651698b3612e173a4b85faccdcb05d4456b667b6
peterwu-cpl <wup@cantonpl.org>
2023-02-21 14:49:15 -0500

    uploads parquet file instead of csv

--- clean_upload_append.bat
+++ clean_upload_append.bat
@@ -7,6 +7,6 @@ set FILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%
 
 call E:\APPLICATIONS\MATERIALS\venv\Scripts\activate.bat 
 E:\APPLICATIONS\MATERIALS\data_collector\circ_data_cleaner.py > Logs\%FILENAME%.log 2>&1
-pscp -pw w38t35t! "\\cpl-circserv\SHINY\Circ Shiny Dashboard\Data\CPLCircShinyDashboardData\CPL Circ Data.csv" "ibranch@sat.cantonpl.org:/var/www/html/shiny" >> Logs\%FILENAME%.log 2>&1
+pscp -pw w38t35t! "\\cpl-circserv\SHINY\Circ Shiny Dashboard\Data\CPLCircShinyDashboardData\CPL Circ Data.parquet" "ibranch@sat.cantonpl.org:/var/www/html/shiny" >> Logs\%FILENAME%.log 2>&1
 E:\APPLICATIONS\MATERIALS\data_collector\circ_data_collector.py >> Logs\%FILENAME%.log 2>&1
 call E:\APPLICATIONS\MATERIALS\venv\Scripts\deactivate.bat
